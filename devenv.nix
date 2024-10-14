aa05e41aa012662f17fa197adc1dbe27b04e37ea
Marcus Ramberg <marcus.ramberg@remarkable.no>
2024-10-14 13:30:36 +0200

    chore: setup devenv

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  pre-commit = {
+    hooks = {
+      gofmt.enable = true;
+      shellcheck.enable = true;
+      markdownlint.enable = true;
+      yamllint.enable = true;
+      commitizen.enable = true;
+    };
+  };
+  # https://devenv.sh/packages/
+  packages = [ pkgs.go-task ];
+  languages.go.enable = true;
+}
